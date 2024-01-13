509efa6228f935bbdef02588593f167beca98f61
Aaron Glaser <aaron.glaser.05@gmail.com>
2024-01-13 01:43:52 +0100

    .

---
+++ CodeforceDaily/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:CodeforceDaily/providers/ApiKeyProvider.dart';
+import 'package:CodeforceDaily/providers/CurrentRankProvider.dart';
+import 'package:CodeforceDaily/providers/ProblemProvider.dart';
+import 'package:CodeforceDaily/providers/StreakProvider.dart';
+import 'package:CodeforceDaily/screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import 'api/PseudoRandomNumber.dart';
+import 'screens/CongratulationScreen.dart';
+
+void main(List<String> args) async {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => CurrentRank()),
+      ChangeNotifierProvider(create: (_) => ApiKeyProvider()),
+      ChangeNotifierProvider(create: (_) => StreakProvider()),
+      ChangeNotifierProvider(create: (_) => ProblemProvider())
+    ],
+    child: const App(),
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final streakProvider = Provider.of<StreakProvider>(context, listen: false);
+
+    return FutureBuilder(
+        future: streakProvider.initializeStreak(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ScreenUtilInit(
+              minTextAdapt: true,
+              splitScreenMode: true,
+              builder: (_, child) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Flutter Demo',
+                  initialRoute: streakProvider.lastIncrease != daysSinceUnix
+                      ? '/'
+                      : '/congratulation',
+                  routes: {
+                    '/': (context) => const HomeScreen(),
+                    '/congratulation': (context) => const CongratulationScreen()
+                  },
+                  theme: ThemeData(
+                    popupMenuTheme: const PopupMenuThemeData(
+                      color: Colors.transparent, // Set to transparent
+                      // You can also add other styling properties if needed
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
+  }
+}
